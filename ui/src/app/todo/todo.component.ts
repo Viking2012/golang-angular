36ffc4903f932e5a4e6d8e120285a9f7b32a6c36
Alexander Orban <viking2012@gmail.com>
2020-07-13 20:03:49 -0400

    create realistic todo components

--- ui/src/app/todo/todo.component.ts
+++ ui/src/app/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService, Todo } from '../todo.service';
 
 @Component({
   selector: 'app-todo',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
-  constructor() { }
+  activeTodos: Todo[];
+  completedTodos: Todo[];
+  todoMessage: string;
+
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
+    this.getAll();
   }
+
+  getAll(): void {
+    this.todoService.getTodoList().subscribe((data:Todo[]) => {
+      this.activeTodos = data.filter((a) => !a.complete);
+      this.completedTodos = data.filter((a) => a.complete);
+    });
+  }
+
+  addTodo():void {
+    var newTodo: Todo = {
+      message: this.todoMessage,
+      id:'',
+      complete: false
+    };
+
+    this.todoService.addTodo(newTodo).subscribe(() => {
+      this.getAll();
+      this.todoMessage = '';
+    });
+  }
+
+  completeTodo(todo: Todo) {
+    this.todoService.completeTodo(todo).subscribe(() => {
+      this.getAll();
+    });
+  }
+
+  deleteTodo(todo: Todo) {
+    this.todoService.deleteTodo(todo).subscribe(() => {
+      this.getAll()
+    });
+  }
+
 
 }
