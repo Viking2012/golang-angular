edd01f9bac86533dbf9b75cc0445719c7bee4a3a
Alexander Orban <viking2012@gmail.com>
2020-07-19 16:03:54 -0400

    new callback component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { TodoComponent } from './todo/todo.component';
+import { CallbackComponent } from './callback/callback.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TodoComponent
+    TodoComponent,
+    CallbackComponent
   ],
   imports: [
     BrowserModule
