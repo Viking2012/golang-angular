8f7ba7405800e7de03b54c333f425367a62bcb86
Alexander Orban <viking2012@gmail.com>
2020-07-19 16:52:47 -0400

    checked if auth'd - if not, route to login

--- ui/src/app/auth-guard.service.ts
+++ ui/src/app/auth-guard.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from "@angular/router";
+import { AuthService } from "./auth.service";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AuthGuardService {
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
+
+  canActivate(): boolean {
+    if (this.auth.isAuthenticated()) {
+      return true;
+    }
+
+    this.auth.login()
+  }
 }
