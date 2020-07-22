13e4cc7d2cbf6ad31536e5df6f6153f4bd39f4b9
Alexander Orban <viking2012@gmail.com>
2020-07-22 15:01:47 -0400

    start with fresh angular project

--- ui/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  gateway:'',
-  callback:'http://localhost:4200/callback',
-  domain:'https://dev-h3ku2y7p.us.auth0.com',
-  clientID:'5f0cf842dcf37e0031b5ac92',
-  audience:'https://my-golang-api'
-};
