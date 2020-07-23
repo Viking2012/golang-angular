5696c59624c408daa523a69a95b685257ed85eac
Alexander Orban <viking2012@gmail.com>
2020-07-22 22:08:07 -0400

    synchronous service call

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Hero } from "./heros/hero";
+import { HEROS } from "./mocks/heros";
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,8 @@ import { Injectable } from '@angular/core';
 export class HeroService {
 
   constructor() { }
+  
+  getHeros(): Hero[] {
+    return HEROS;
+  }
 }
--- angular-tour-of-heroes/src/app/heros/heros.component.ts
+++ angular-tour-of-heroes/src/app/heros/heros.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from "./hero";
 import { HEROS } from "../mocks/heros";
 
+import { HeroService } from "../hero.service";
+
 @Component({
   selector: 'app-heros',
   templateUrl: './heros.component.html',
@@ -9,16 +11,25 @@ import { HEROS } from "../mocks/heros";
 })
 export class HerosComponent implements OnInit {
 
-  heros = HEROS;
+  heros: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeros();
   }
 
   onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+    if (hero === this.selectedHero) {
+      this.selectedHero = null;
+    } else {
+      this.selectedHero = hero;
+    }
+  }
+
+  getHeros(): void {
+    this.heros = this.heroService.getHeros();
   }
 
 }
